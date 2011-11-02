3e2c2051838c6ffb408f35bff49dacd0f2f89e97
3on <jr@3on.fr>
2011-11-02 05:06:34 +0100

    Tchou tchou makes the train moving forward

---
+++ www/style.js
@@ -0,0 +1,12 @@
+function __$__ () {
+  var h = $(window).height() - $("#header").first().outerHeight(true) - $(".infos").first().outerHeight(true)
+  $(".left .newsFeed").height(h - 40)
+  
+  h -= $(".right .tabs-head").outerHeight(true)
+  $(".right .tabs-body").height(h - 20)
+}
+
+$(function() {
+  $(window).resize(__$__)
+  __$__()
+})
