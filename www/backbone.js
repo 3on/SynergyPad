44a4016f506f7aa467d5ca48eed730e91e0330d3
3on <jr@3on.fr>
2011-11-11 18:07:01 +0100

    nothing really

--- www/backbone.js
+++ www/backbone.js
@@ -1,14 +1,42 @@
 function models () {
-  window.Feed = Backbone.Model.extend({
+/*
+  window.Comment = Backbone.Model.extend({
     default: function() {
       return {
-        comments: [],
-        
+        author: "John Doe",
+        text: "This is a comment !",
+        id: 42
       }
     }
   })
+  
+  window.CommentsList = Backbone.Collection.extend({
+    model: Comment
+  })
+*/ 
+  window.Post = Backbone.Model.extend({
+    default: function() {
+      return {
+        //comments: CommentsList,
+        text: 'Dumb Post',
+        liked: false,
+      }
+    }
+  })
+  
+  window.PostList = Backbone.Model.extend({
+    model: Post
+  })
+  
+}
+
+function appviews() {
+  
 }
 
 $(function(){
   models()
+  appview();
+  
+  window.App = new AppView()
 })
